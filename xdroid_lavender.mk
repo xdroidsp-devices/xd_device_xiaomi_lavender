daa691e30f1031bfb4e441a91946d72cc0bd5155
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-11 18:23:20 +0200

    lavender: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: If00badd6dc12e0245c74bba12e4816361091e754
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- xdroid_lavender.mk
+++ xdroid_lavender.mk
@@ -7,7 +7,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common xdroidsp stuff
 $(call inherit-product, vendor/xdroid/config/common.mk)
