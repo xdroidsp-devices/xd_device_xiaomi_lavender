013de41a9fddac74f6eb635ed6c053d6d661b2ad
xyzuan <xyzuannihboss@gmail.com>
2022-02-06 05:24:05 +0000

    lavender: disable Surfaceflinger backpressure

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -209,7 +209,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    debug.sf.disable_backpressure=1
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
