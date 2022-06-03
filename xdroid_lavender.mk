5bed197cb9e8726eeab8dfc08039476c5e304823
xyzuan <xyzuannihboss@gmail.com>
2022-06-03 12:46:44 +0000

    lavender: Re-enable Quick Tap Columbus support
    This reverts commit 40ee0b08b986c3d8a8277a6594542425ebb1b0b1.

--- xdroid_lavender.mk
+++ xdroid_lavender.mk
@@ -16,7 +16,7 @@ XDROID_MAINTAINER := xyzuan
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
