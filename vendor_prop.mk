b0c055d430f706f24ed45235ec2a762c2b9f24f3
xyzuan <xyzuannihboss@gmail.com>
2022-06-17 19:47:57 +0700

    lavender: [REVERT] AOD, Doze changes

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -137,7 +137,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.enable_default_color_mode=0 \
     ro.vendor.display.cabl=0 \
-    debug.force_no_blanking=true \
     debug.sdm.support_writeback=0
 
 # DRM
